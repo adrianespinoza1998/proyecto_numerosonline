ce0d31e172a3d44eb590ab6a9beeb2ac9407f2b2
Adrian Espinoza <musrambo.espinoza@gmail.com>
2019-09-02 23:45:41 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    //Fade in Body
+    $('body').css('display','none');
+    $('body').fadeIn(1000);
+    //$('body').fadeOut(1000);
+
+    $('#btnIngresar').click(function () {
+        var email=$('#mail').val();
+        var password=$('#password').val();
+
+        if(email=='' || password==''){
+            alert('Uno o mas campos vacios');
+        }else{
+            if(email=='correo@prueba' && password=='12345'){
+                alert('Usuario correcto');
+            }else{
+                alert('Correo o contraseña incorrectos');
+            }
+        }
+    });
+});
