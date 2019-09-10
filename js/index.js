42f71b5436760b47d4ea7cb083431a9d3a19094b
adrianespinoza1998 <adrianespinozaarevalo@gmail.com>
2019-09-09 21:24:30 -0300

    Se agrega pagina de inicio

--- js/index.js
+++ js/index.js
@@ -1,21 +1,6 @@
 $(document).ready(function () {
-    //Fade in Body
-    $('body').css('display','none');
-    $('body').fadeIn(1000);
-    //$('body').fadeOut(1000);
-
-    $('#btnIngresar').click(function () {
-        var email=$('#mail').val();
-        var password=$('#password').val();
-
-        if(email=='' || password==''){
-            alert('Uno o mas campos vacios');
-        }else{
-            if(email=='correo@prueba' && password=='12345'){
-
-            }else{
-                alert('Correo o contraseña incorrectos');
-            }
-        }
+    $('btnIngresar').click(function () {
+        alert('Metodo funcionando');
+        location.href='html/login.html';
     });
 });
