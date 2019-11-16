bc07750a9127f54ea766e0d15d06478e0e75e398
Adrian Espinoza <musrambo.espinoza@gmail.com>
2019-11-16 16:59:45 -0300

    Se añaden valores por defecto

--- backend/Datos/admin.py
+++ backend/Datos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Usuario)
+admin.site.register(Region)
+admin.site.register(Ciudad)
+admin.site.register(Comuna)
+admin.site.register(Notaria)
+admin.site.register(Tramite)
+admin.site.register(Fila)
+admin.site.register(Ticket)
 
 # Register your models here.
--- backend/Datos/models.py
+++ backend/Datos/models.py
@@ -3,47 +3,47 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
-    id_usuario:models.IntegerField
-    nombre:models.CharField(max_length=50)
-    ap_paterno:models.CharField(max_length=50)
-    ap_materno:models.CharField(max_length=50)
-    rut:models.CharField(max_length=9)
-    correo:models.CharField(max_length=50)
-    contrasena:models.CharField(max_length=50)
+    id_usuario=models.IntegerField(default=0)
+    nombre=models.CharField(max_length=50,default='Sin nombre')
+    ap_paterno=models.CharField(max_length=50,default='Sin ap paterno')
+    ap_materno=models.CharField(max_length=50,default='Sin ap materno')
+    rut=models.CharField(max_length=9,default='Sin rut')
+    correo=models.CharField(max_length=50,default='No hay correo')
+    contrasena=models.CharField(max_length=50,default='Sin contraseña')
 
 class Region(models.Model):
-    id_region:models.IntegerField
-    nombreRegion:models.CharField(max_length=50)
+    id_region=models.IntegerField(default=0)
+    nombreRegion=models.CharField(max_length=50,default='Sin nombre')
 
 class Ciudad(models.Model):
-    id_ciudad:models.IntegerField
-    id_region:models.IntegerField
-    nombreCiudad:models.CharField(max_length=50)
+    id_ciudad=models.IntegerField(default=0)
+    id_region=models.IntegerField(default=0)
+    nombreCiudad=models.CharField(max_length=50,default='Sin nombre')
 
 class Comuna(models.Model):
-    id_comuna:models.IntegerField
-    id_ciudad:models.IntegerField
-    nombreComuna:models.CharField(max_length=50)
+    id_comuna=models.IntegerField(default=0)
+    id_ciudad=models.IntegerField(default=0)
+    nombreComuna=models.CharField(max_length=50,default='Sin nombre')
 
 class Notaria(models.Model):
-    id_notaria:models.IntegerField
-    id_comuna:models.IntegerField
-    nombre_notaria:models.CharField(max_length=50)
-    direccion:models.CharField(max_length=50)
-    telefono:models.IntegerField
+    id_notaria=models.IntegerField(default=0)
+    id_comuna=models.IntegerField(default=0)
+    nombre_notaria=models.CharField(max_length=50,default='Sin nombre')
+    direccion=models.CharField(max_length=50,default='Sin dirección')
+    telefono=models.IntegerField(default=0)
 
 class Tramite(models.Model):
-    id_tramite:models.IntegerField
-    nombreTramite:models.CharField(max_length=50)
+    id_tramite=models.IntegerField(default=0)
+    nombreTramite=models.CharField(max_length=50,default='Sin nombre')
 
 class Fila(models.Model):
-    id_fila:models.IntegerField
-    id_notaria:models.IntegerField
-    id_tramite:models.IntegerField
-    nroFila:models.IntegerField
+    id_fila=models.IntegerField(default=0)
+    id_notaria=models.IntegerField(default=0)
+    id_tramite=models.IntegerField(default=0)
+    nroFila=models.IntegerField(default=0)
 
 class Ticket(models.Model):
-    id_ticket:models.IntegerField
-    id_usuario:models.IntegerField
-    id_fila:models.IntegerField
+    id_ticket=models.IntegerField(default=0)
+    id_usuario=models.IntegerField(default=0)
+    id_fila=models.IntegerField(default=0)
 
